38feb63cd5ea679ac7d0b9f4dcbc33d7e66cea69
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-02-10 22:25:25 +0100

    Part III - Advanced Routes: Return Combined Data

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -5,12 +5,12 @@ const asyncHandler=require('express-async-handler');
 const getPhotos=asyncHandler(async(req,res)=>{
    try{
       const {data}=await axios.get(`https://api.unsplash.com/photos/?client_id=${process.env.UNSPLASH_ACCESS_KEY}`);
-      const urls=data.map(i=>{
+/*       const urls=data.map(i=>{
          return {
             raw:i.urls.raw
          }
-      });
-      res.status(200).send(urls);
+      }); */
+      res.status(200).send(data);
    }catch(err){
       res.status(500).json('Server error. Please try again later.');
    }
@@ -26,7 +26,27 @@ const getPhoto=asyncHandler(async(req,res)=>{
    }
 })
 
+const getUserPhotos=asyncHandler(async(req,res)=>{
+   const {username}=req.params;
+   try{
+      const {data}=await axios.get(`https://api.unsplash.com/users/${username}/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}`)
+      const userPhotos=data.map(i=>{
+         return {
+            id:i.id,
+            username:i.user.username,
+            description:i.description?i.description:'No description provided.',
+            url:i.urls.raw,
+
+         }
+      })
+      res.status(200).json(userPhotos);   
+   }catch(err){
+      res.status(200).json({message:err.error});
+   }
+})
+
 module.exports={
    getPhoto,
-   getPhotos
+   getPhotos,
+   getUserPhotos
 }
--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const router=express.Router();
-const {getPhoto,getPhotos}=require('../controllers/photoController');
+const {
+   getPhoto,
+   getPhotos,
+   getUserPhotos}=require('../controllers/photoController');
 
 router.route('/').get(getPhotos);
 router.route('/:id').get(getPhoto);
+router.route('/user/:username').get(getUserPhotos);
 
 module.exports=router;
